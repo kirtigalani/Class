3e769f21a9e29009e222dc1fc319492da97a5642
kirtigalani <kittugalani@gmail.com>
2024-12-04 18:13:20 +0530

    Add project files.

---
+++ Class/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+    class Customer
+    {
+        string _first;
+        string _last;
+
+        public void customer(string firstName, string lastName)
+        {
+            this._first = firstName;
+            this._last = lastName;
+        }
+        public void print()
+        {
+            Console.WriteLine("Full name {0} ", this._first+" "+ this._last);
+        }
+        ~Customer()
+        {
+            //clean up code
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer c1 = new Customer("Hello", "world");
+            c1.print();
+        }
+    }
